c9c080353183ce9a28a3390776244d06913596f5
Laura8720 <abbatilaura20@gmail.com>
2025-11-05 11:41:04 +0100

    inserimento classe SynchronizedBlockExample e adattamento classe Main ai due tipi di contatore

---
+++ src/SynchronizedBlockExample.java
@@ -0,0 +1,15 @@
+public class SynchronizedBlockExample {
+    private final Object lock = new Object();
+    private int count = 0;
+
+    public void increment() {
+        synchronized (lock) {
+            count++;
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
